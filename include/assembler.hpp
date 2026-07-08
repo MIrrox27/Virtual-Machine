966645a928f3025706ab6fb759e81b9ea021dcff
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-08 15:50:24 +0500

    добавлены функции

--- include/assembler.hpp
+++ include/assembler.hpp
@@ -4,5 +4,11 @@
 #ifndef ASSEMBLER_HPP
 #define ASSEMBLER_HPP
 
+#include <vector>
+#include <string>
+
+std::vector<int> get(std::string path); // получение байт из файла 
+void assembly(); // если расширение не .axn то пересобирает в новый исполняемый файл с числами
+void execute(); // выполнить код (передаются байты)
 
 #endif
