b6573724e788b46b788c262893d318332d9fb88d
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-09 16:10:41 +0500

    добавлен новый тег

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,26 @@ int main(int argc, char* argv[]){
      * index = 1:
         "-r" - run bytecode
         "-c" - compile txt commands to bytecode
+        "-cr" - compile txt commands to bytecode and run
       * index = 2:
         ```path``` - path to file with  bytecode
   */
 
   if (argc > 3 && argv[1] == "-r"){ // если тег "-r" то будут читаться и выполняться байты
-
-  }
-  else if (argc > 3 && argv[1] == "-c"){ // если тег "-c" то команда сначала переведется в байт код (в отдельный файл) и только потом запустится 
-
+    std::vector<int> bytes = get(argv[2]);
+    execute(bytes);
   }
 
+  else if (argc > 3 && argv[1] == "-cr"){ // если тег "-cr" то команда сначала переведется в байт код (в отдельный файл) и только потом запустится 
+    std::string path = assembly(argv[2]);
+    std::vector<int> bytes = get(path);
+    execute(bytes);
+
+  }
+
+  else if (argc > 3 && argv[1] == "-c")
+    std::string path = assembly(argv[2]);
+  
   //else repl();
   
 
