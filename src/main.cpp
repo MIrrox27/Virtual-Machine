5b61076775ea9ce9db73640888815d34d364f4d1
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-11 13:21:45 +0500

    добавлены выводы, изменена структура тегов

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 
 int main(int argc, char* argv[]){
   
-  
+  std::cout << "Virtual Machine. Author: github.com/MIrrox27/Virtual-Machine" <<std::endl;
   /*
     Args 
      * index = 1:
@@ -25,22 +25,22 @@ int main(int argc, char* argv[]){
         ```path``` - path to file with  bytecode
   */
 
-  if (argc > 3 && argv[1] == "-r"){ // если тег "-r" то будут читаться и выполняться байты
+  if (argc >= 3 && argv[1] == "-r"){ // если тег "-r" то будут читаться и выполняться байты
     std::vector<int> bytes = get(argv[2]);
     execute(bytes);
   }
 
-  else if (argc > 3 && argv[1] == "-cr"){ // если тег "-cr" то команда сначала переведется в байт код (в отдельный файл) и только потом запустится 
+  else if (argc >= 3 && argv[1] == "-cr"){ // если тег "-cr" то команда сначала переведется в байт код (в отдельный файл) и только потом запустится 
     std::string path = assembly(argv[2]);
     std::vector<int> bytes = get(path);
     execute(bytes);
 
   }
 
-  else if (argc > 3 && argv[1] == "-c")
+  else if (argc >= 3 && argv[1] == "-c")
     std::string path = assembly(argv[2]);
   
-  //else repl();
+  else repl();
   
 
   return 0;
